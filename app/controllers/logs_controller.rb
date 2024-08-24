8a0cdbb42d9f0f2c13ac2862a854f72914a9d8ea
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-24 17:56:27 -0300

    feat: implements parse action in Logs controller

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,4 +1,8 @@
 class LogsController < ApplicationController
   def parse
+    log_file_path = File.join("lib", "assets", "qgames.log")
+    log_parser = LogParser.new(log_file_path)
+    # puts log_parser
+    @logs = log_parser.parse
   end
 end
