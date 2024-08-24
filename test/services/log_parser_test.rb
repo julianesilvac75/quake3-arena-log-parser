f68617c9cbd3ca0363ae434c102b29714a6ed31a
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-24 16:05:40 -0300

    tests: creates 'should parse logs and return array of logs'

--- test/services/log_parser_test.rb
+++ test/services/log_parser_test.rb
@@ -1,5 +1,14 @@
 require "test_helper"
 
 class LogParserTest < ActiveSupport::TestCase
+  def setup
+    @log_file_path = Rails.root.join("test", "fixtures", "files", "qgames.log")
+    @log_parser = LogParser.new
+  end
 
+  test "should parse logs and return array of logs" do
+    logs = @log_parser.parse
+    assert_kind_of Array, logs, "Log Parser didn't return an array of logs"
+    assert_equal 57, logs.size, "Log size didn't match"
+  end
 end
