15a1b92aade8f2e4ec8c3d4f0596001042679ea1
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-25 20:39:16 -0300

    tests: refactor matches controller tests

--- test/controllers/matches_controller_test.rb
+++ test/controllers/matches_controller_test.rb
@@ -1,14 +1,17 @@
 require "test_helper"
+require "mocha/minitest"
 
 class MatchesControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    get matches_path
-  end
 
   test "should get to the matches page" do
+    stubbed_object = [ "object1", "object2" ]
+    Match.expects(:all).returns(stubbed_object)
+
+    get matches_path
+
     assert_response :success
 
-    assert_select "div", "Total of Matches: 2", "Should show corret total of matches"
+    assert_select "div", "Total of Matches: 2"
     assert_select "a", "Back to logs", "Should have a link with text 'Back to logs'"
   end
 end
