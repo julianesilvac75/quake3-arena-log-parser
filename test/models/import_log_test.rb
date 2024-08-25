ccb6075b9a5520aabde14a08794223b9411b4400
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-25 11:25:30 -0300

    test: create model test 'should create a new import object with imported value being true'

--- test/models/import_log_test.rb
+++ test/models/import_log_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class ImportLogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create a new import object with imported value being true" do
+    @log_file_path = Rails.root.join("test", "fixtures", "files", "qgames.log").to_s
+    new_import = ImportLog.new(source_file: @log_file_path, imported: true)
+
+    assert new_import.save, "New import should be saved successfully"
+    assert new_import.is_a?(ImportLog), "New import should be an object of ImportLog class"
+    assert_equal new_import.source_file, @log_file_path, "New import should have the right source file path"
+    assert_equal new_import.imported, true, "New imported should have 'imported' attribute 'true'"
+  end
 end
