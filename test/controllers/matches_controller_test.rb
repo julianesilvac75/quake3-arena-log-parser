1b5afd0a6c8891672217f58c2f40c7473e443984
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-25 19:51:49 -0300

    tests: creates test for Matches controller

--- test/controllers/matches_controller_test.rb
+++ test/controllers/matches_controller_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class MatchesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    get matches_path
+  end
+
+  test "should get to the matches page" do
+    assert_response :success
+
+    assert_select "div", "Total of Matches: 2", "Should show corret total of matches"
+    assert_select "a", "Back to logs", "Should have a link with text 'Back to logs'"
+  end
 end
