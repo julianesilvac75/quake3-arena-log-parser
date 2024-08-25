4750de413d506ecfa732778a99d376c5bfd0b509
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-25 14:38:47 -0300

    feat: implements logic to the LogImporter import method

--- app/services/log_importer.rb
+++ app/services/log_importer.rb
@@ -2,9 +2,6 @@ class LogImporter
   def initialize(log_file_path)
     @log_file_path = log_file_path
   end
-  def import
-    true
-  end
 
   def self.find_file(file_path)
     @file = ImportLog.where(source_file: @log_file_path)
@@ -13,6 +10,17 @@ class LogImporter
       @file.first
     else
       false
+    end
+  end
+
+  def import
+    imported_file = self.class.find_file(@log_file_path)
+
+    if !imported_file
+      result = ImportLog.create(source_file: @log_file_path, imported: true)
+      "Log data was imported successfully. Source file: #{result.source_file}"
+    else
+      "Log data was already imported."
     end
   end
 end
