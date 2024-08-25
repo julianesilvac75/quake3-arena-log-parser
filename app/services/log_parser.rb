c940513788be4042a0dbc9df98d05ab55edec3be
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-25 20:54:23 -0300

    refactor: separate filter logs logic from adding info to the database logic

--- app/services/log_parser.rb
+++ app/services/log_parser.rb
@@ -7,18 +7,18 @@ class LogParser
   end
 
   def parse
-    logs = []
-    File.foreach(@log_file_path) do |line|
+    logs = filter_logs
+
+    logs.each do |line|
       if line.include?("InitGame:")
         create_match
-        logs << line
-      else
-        filtered_line = filter_log(line)
-        if filtered_line
-          logs << filtered_line
-        end
+      elsif line.include?("ClientUserinfoChanged")
+        "ClientUserinfoChanged"
+      elsif line.include?("Kill:")
+        "kill"
       end
     end
+
     logs
   end
 
@@ -29,17 +29,18 @@ class LogParser
       new_match
   end
 
-  private
+  def filter_logs
+    @logs_categories = [ "InitGame:", "ClientUserinfoChanged:", "Kill:" ]
+    logs = []
 
-  def filter_log(log)
-    @logs_categories = [ "ClientUserinfoChanged:", "Kill:" ]
-
-    @logs_categories.each do |category|
-      if log.include?(category)
-        return log
+    File.foreach(@log_file_path) do |line|
+      @logs_categories.each do |category|
+        if line.include?(category)
+          logs << line
+        end
       end
     end
 
-    nil
+    logs
   end
 end
