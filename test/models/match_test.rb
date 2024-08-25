551ec87d312ae1b71dc019964170cd210aa42cb9
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-24 21:57:28 -0300

    tests-refactor: adds one more assert to verify if match id is a number

--- test/models/match_test.rb
+++ test/models/match_test.rb
@@ -3,7 +3,9 @@ require "test_helper"
 class MatchTest < ActiveSupport::TestCase
   test "should create a match with valid id" do
     match = Match.new
+
     assert match.save, "Match should be saved successfully"
     assert match.id.present?, "Match should have an id"
+    assert_kind_of Integer, match.id, "Match should have an id that is a number"
   end
 end
