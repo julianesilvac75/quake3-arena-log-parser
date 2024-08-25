41b112f365fc6067fd4fe37fea623bdc436ceadb
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-25 20:12:51 -0300

    test: refactor test for log importer class

--- test/services/log_importer_test.rb
+++ test/services/log_importer_test.rb
@@ -3,47 +3,48 @@ require "mocha/minitest"
 
 class LogImporterTest < ActiveSupport::TestCase
   def setup
-    @log_file_path = Rails.root.join("test", "fixtures", "files", "qgames.log").to_s
+    @log_file_path = Rails.root.join("test", "fixtures", "files", "qgames.log")
     @new_import = LogImporter.new(@log_file_path)
   end
 
   test "should return a log file registry if it exists" do
-    @stubbed_object = ImportLog.new(source_file: @log_file_path, imported: true)
+    @stubbed_object = ImportLog.new(source_file: @log_file_path)
 
     ImportLog.expects(:where).returns([ @stubbed_object ])
 
-    result = LogImporter.find_file(@log_file_path)
+    result = @new_import.find_file
 
     assert_equal @stubbed_object, result, "Should return the import log registry"
     assert_kind_of ImportLog, result
   end
 
   test "should return false if the log file registry doesn't exists" do
+    new_import2 = LogImporter.new(source_file: "non/existent/log_file.log")
     ImportLog.expects(:where).returns([])
 
-    result = LogImporter.find_file("non/existent/log_file.log")
+    result = new_import2.find_file
 
     assert_equal false, result, "Should return false"
   end
 
   test "should not import log if it already exists" do
-    @stubbed_object = ImportLog.new(source_file: @log_file_path, imported: true)
+    @stubbed_object = ImportLog.new(source_file: @log_file_path)
 
     ImportLog.expects(:where).returns([ @stubbed_object ])
 
-    result = @new_import.import
-
-    assert_equal "Log data was already imported.", result, "Should not import log data if it already exists"
+    assert_no_difference "ImportLog.count" do
+      @new_import.import
+    end
   end
 
   test "should import log if it doesn't exists" do
-    @stubbed_object = ImportLog.new(source_file: @log_file_path, imported: true)
+    @stubbed_object = ImportLog.new(source_file: @log_file_path)
 
     ImportLog.expects(:where).returns([])
     ImportLog.expects(:create).returns(@stubbed_object)
 
-    result = @new_import.import
-
-    assert_equal "Log data was imported successfully. Source file: #{@stubbed_object.source_file}", result, "Should import log data successfully"
+    assert_difference "ImportLog.count", 1 do
+      @new_import.import
+    end
   end
 end
