f1a08e2371838b624c3ca4ce94e08c17b2d42fd9
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-24 21:51:09 -0300

    tests: creates test 'should create a match with valid id'

--- test/models/match_test.rb
+++ test/models/match_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class MatchTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create a match with valid id" do
+    match = Match.new
+    assert match.save, "Match should be saved successfully"
+    assert match.id.present?, "Match should have an id"
+  end
 end
