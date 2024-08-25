8887569e93d666b1b70c062bf023d8b675fc7b0b
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-24 22:35:52 -0300

    feat: implements logic to create a new match in the db when a new match starts

--- app/services/log_parser.rb
+++ app/services/log_parser.rb
@@ -1,24 +1,38 @@
 class LogParser
+  attr_reader :current_match
+
   def initialize(log_file_path)
     @log_file_path = log_file_path
+    @current_match = 0
   end
 
   def parse
     logs = []
     File.foreach(@log_file_path) do |line|
-      filtered_line = filter_log(line)
-      if filtered_line
-        logs << filtered_line
+      if line.include?("InitGame:")
+        create_match
+        logs << line
+      else
+        filtered_line = filter_log(line)
+        if filtered_line
+          logs << filtered_line
+        end
       end
     end
 
     logs
   end
+  def create_match
+    new_match = Match.create
+    @current_match = new_match.id
+
+    new_match
+  end
 
   private
 
   def filter_log(log)
-    @logs_categories = [ "InitGame:", "ClientUserinfoChanged:", "Kill:" ]
+    @logs_categories = [ "ClientUserinfoChanged:", "Kill:" ]
 
     @logs_categories.each do |category|
       if log.include?(category)
