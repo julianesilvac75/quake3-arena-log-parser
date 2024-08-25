a86a91a5c76a341add3d09c45bf646ac32f2c894
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-24 22:58:58 -0300

    feat: shows total matches number on the /log/parse page

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,8 +1,12 @@
 class LogsController < ApplicationController
+  def initialize
+    @matches = Match.all
+  end
+
   def parse
     log_file_path = File.join("lib", "assets", "qgames.log")
     log_parser = LogParser.new(log_file_path)
-    # puts log_parser
     @logs = log_parser.parse
+    @matches
   end
 end
