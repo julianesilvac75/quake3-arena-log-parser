2f5383aadd6798a8382524848f9d852d6601486c
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-25 19:41:22 -0300

    tests: refactor Logs controller tests

--- test/controllers/logs_controller_test.rb
+++ test/controllers/logs_controller_test.rb
@@ -3,23 +3,14 @@ require "mocha/minitest"
 
 class LogsControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @log_parser_mock = mock("LogParser")
-    @logs_mock = YAML.load_file(
-      Rails.root.join("test", "fixtures", "files", "game_logs_mock.yml")
-    )["game_logs"]
-
-    @log_parser_mock.stubs(:parse).returns(@logs_mock)
-
-    LogParser.stubs(:new).returns(@log_parser_mock)
-
-    get logs_parse_url
+    get logs_path
   end
 
-  test "should get parsed logs" do
+  test "should get to the home page" do
     assert_response :success
 
-    assert_select "h1", "Game Logs", "Game Logs title is not on the page"
-    assert_select "li", "0:00 InitGame", "Logs are not on the page"
-    assert_select "li", "1:26 Kill", "Logs are not on the page"
+    assert_select "h1", "Logs#Index", "Should have a title"
+
+    assert_select "a", "Matches", "Should have a link to the Matches page"
   end
 end
