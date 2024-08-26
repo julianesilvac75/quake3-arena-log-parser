e8a396a61188f94c9a27b43197399201a6f93e0d
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-25 23:27:12 -0300

    tests: creates tests for players controller

--- test/controllers/players_controller_test.rb
+++ test/controllers/players_controller_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
+require "mocha/minitest"
 
 class PlayersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @mocked_objects = [ Player.new(name: "John"), Player.new(name: "Mary")]
+  end
+
+  test "should get to the players page" do
+    Player.expects(:all).returns(@mocked_objects)
+
+    get players_path
+
+    assert_response :success
+
+    assert_select "h1", "Players"
+    assert_select "li", "John"
+    assert_select "li", "Mary"
+  end
 end
