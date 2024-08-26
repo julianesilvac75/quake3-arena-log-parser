551bd2b5c9e3983531d2081f77cde9491ca6599c
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-25 22:28:48 -0300

    tests: creates test for Player model

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class PlayerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create new player" do
+    new_player = Player.new(name: "John")
+
+    assert new_player.save, "Should have saved the player successfully"
+    assert_equal "John", new_player.name, "Player's name should match the given name"
+  end
 end
