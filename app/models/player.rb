ccbcae4d7383aea8d44a825c4e0b54bd3b338189
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-26 12:12:50 -0300

    feat: creates a update_name method for the player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,19 @@
 class Player < ApplicationRecord
   has_many :matches_players
   has_many :matches, through: :matches_players
+
+  validates :name, presence: true
+
+  def update_name(new_name)
+    if Player.exists?(name: new_name)
+      errors.add(:name, "New name is already in use")
+      false
+    else
+      update(name: new_name)
+    end
+
+    rescue ActiveRecord::RecordNotUnique
+      errors.add(:name, "Name wasn't updated because of a uniqueness constraint")
+      false
+  end
 end
