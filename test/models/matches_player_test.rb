d3ad19f169dd66d2a2b00f07f67c49ed26626004
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-26 10:43:44 -0300

    tests: creates test for matches_players model

--- test/models/matches_player_test.rb
+++ test/models/matches_player_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class MatchesPlayerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @match = Match.create!
+    @player = Player.create!(name: "John")
+    @matches_player = MatchesPlayer.new(match: @match, player: @player, assigned_id: 1)
+  end
+  test "should be valid with valid attributes" do
+    assert @matches_player.valid?
+  end
+
+  test "should require a match" do
+    @matches_player.match = nil
+    assert_not @matches_player.valid?
+  end
+
+    test "should require a player" do
+      @matches_player.player = nil
+      assert_not @matches_player.valid?
+    end
+
+  test "should require an assigned_id" do
+    @matches_player.assigned_id = nil
+    assert_not @matches_player.valid?
+  end
+
+  test "should belong to match and player" do
+    assert_equal @match, @matches_player.match
+    assert_equal @player, @matches_player.player
+  end
 end
