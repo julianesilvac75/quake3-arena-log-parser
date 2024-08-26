a604fc9daa825fd4ade5e8d197d56c94fb4b9c8d
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-26 16:06:38 -0300

    tests: refactor matches controller tests, now using fixtures

--- test/controllers/matches_controller_test.rb
+++ test/controllers/matches_controller_test.rb
@@ -2,16 +2,22 @@ require "test_helper"
 require "mocha/minitest"
 
 class MatchesControllerTest < ActionDispatch::IntegrationTest
-
-  test "should get to the matches page" do
-    stubbed_object = [ "object1", "object2" ]
-    Match.expects(:all).returns(stubbed_object)
-
+  fixtures :matches_players, :players, :matches
+  def setup
     get matches_path
-
+  end
+  test "should get to the matches page" do
     assert_response :success
 
-    assert_select "div", "Total of Matches: 2"
+    assert_select "p", "Total of Matches: 2"
     assert_select "a", "Back to logs", "Should have a link with text 'Back to logs'"
+  end
+
+  test "should display matchs details" do
+    assert_select "h2", "Match 1"
+    assert_select "li", "John"
+
+    assert_select "h2", "Match 2"
+    assert_select "li", "Mary"
   end
 end
