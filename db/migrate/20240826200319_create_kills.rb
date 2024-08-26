d4055701b153f842e315a45f23f3116af3ad4c9c
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-26 17:58:58 -0300

    db: fix Kill model structure

--- db/migrate/20240826200319_create_kills.rb
+++ db/migrate/20240826200319_create_kills.rb
@@ -1,10 +1,10 @@
-class CreateKills < ActiveRecord::Migration[7.2]
+class CreateKills < ActiveRecord::Migration[6.1]
   def change
     create_table :kills do |t|
-      t.references :match, null: false, foreign_key: { to_table: :matches }
-      t.references :killer, null: false, foreign_key: { to_table: :players }
+      t.references :match, null: false, foreign_key: true
+      t.references :killer, null: true, foreign_key: { to_table: :players }
       t.references :killed, null: false, foreign_key: { to_table: :players }
-      t.references :death_mean, null: false, foreign_key: { to_table: :death_means }
+      t.references :death_mean, null: false, foreign_key: true
 
       t.timestamps
     end
