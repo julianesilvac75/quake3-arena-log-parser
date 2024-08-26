13b810a6c0aca60628357177a12943622e406a19
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-26 19:54:07 -0300

    tests: create tests for death_mean model

--- test/models/death_mean_test.rb
+++ test/models/death_mean_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class DeathMeanTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create a match with valid id" do
+    death_mean = DeathMean.new(name: "Rocket_splash")
+
+    assert death_mean.save
+    assert_equal death_mean.name, "Rocket_splash"
+    assert_kind_of DeathMean, death_mean
+  end
 end
