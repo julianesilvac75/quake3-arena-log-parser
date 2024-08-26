217d148ab2758a972ef64f979e10d765ab974d5a
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-26 18:03:47 -0300

    feat: creates KillHandler service and implements get_kill_info and handle methods

---
+++ app/services/kill_handler.rb
@@ -0,0 +1,21 @@
+class KillHandler
+  def initialize(line, current_match)
+    @line = line
+    @current_match = current_match
+    @killer = nil
+    @killed = nil
+    @death_mean = nil
+  end
+
+  def handle
+    get_kill_info
+  end
+
+  def get_kill_info
+    match_data = @line.match(/Kill: \d+ \d+ \d+: (.+?) killed (.+?) by (.+)/)
+
+    @killer = match_data[1] == "<world>" ? nil : match_data[1]
+    @killed = match_data[2]
+    @death_mean = match_data[3]
+  end
+end
