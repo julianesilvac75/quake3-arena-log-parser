06d87be6ebf51bd62024f0b21eec498260c18c06
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-26 10:33:46 -0300

    db: adds relationship between players, matches and matches_players

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,4 @@
 class Match < ApplicationRecord
+  has_many :matches_players
+  has_many :players, through: :matches_players
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,4 @@
 class Player < ApplicationRecord
+  has_many :matches_players
+  has_many :matches, through: :matches_players
 end
