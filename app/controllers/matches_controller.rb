8830c540fc34a05ac133318957c5e0f878c9a559
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-26 21:38:15 -0300

    feat: implements logic to redirect to error page when the match is not found

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,9 +1,19 @@
 class MatchesController < ApplicationController
+  before_action :set_match, only: [ :show ]
   def index
     @matches = Match.all
   end
 
   def show
     @match = Match.find(params[:id])
+  end
+
+  private
+
+  def set_match
+    @match = Match.find_by(id: params[:id])
+    unless @match
+      redirect_to not_found_path, alert: "Match não encontrado"
+    end
   end
 end
