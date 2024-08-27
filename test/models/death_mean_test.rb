734433849e65204172f237bbd379ebb1c73d7130
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-27 08:46:43 -0300

    tests: refactor death_mean model tests and fixtures

--- test/models/death_mean_test.rb
+++ test/models/death_mean_test.rb
@@ -2,10 +2,11 @@ require "test_helper"
 
 class DeathMeanTest < ActiveSupport::TestCase
   test "should create a match with valid id" do
-    death_mean = DeathMean.new(name: "Rocket_splash")
+    death_mean = DeathMean.new(name: "Rocket_splash", description: "by a rocket splash")
 
     assert death_mean.save
     assert_equal death_mean.name, "Rocket_splash"
+    assert_equal death_mean.description, "by a rocket splash"
     assert_kind_of DeathMean, death_mean
   end
 end
