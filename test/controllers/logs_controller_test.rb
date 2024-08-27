5eb8b7b4843dccfaf8ba677e3f0026c6c2520c54
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-27 08:26:33 -0300

    tests: refactor logs controller tests

--- test/controllers/logs_controller_test.rb
+++ test/controllers/logs_controller_test.rb
@@ -9,8 +9,11 @@ class LogsControllerTest < ActionDispatch::IntegrationTest
   test "should get to the home page" do
     assert_response :success
 
-    assert_select "h1", "Logs#Index", "Should have a title"
+    assert_select "h1", "Quake 3 Arena", "Should have a title"
+    assert_select "h2", "Matches Index", "Should have a subtitle"
 
     assert_select "a", "Matches", "Should have a link to the Matches page"
+    assert_select "a", "Players", "Should have a link to the Players page"
+    assert_select "a", "Means of Death", "Should have a link to the Dead Means page"
   end
 end
