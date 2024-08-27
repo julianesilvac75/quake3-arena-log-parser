182b361692e3958dc898bc8d35e2e8076dabf8a7
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-27 01:41:55 -0300

    feat: implements new method to add description to each death_mean

--- app/services/kill_handler.rb
+++ app/services/kill_handler.rb
@@ -23,12 +23,40 @@ class KillHandler
     )
   end
 
+  def get_death_mean_description
+    case @death_mean
+    when "MOD_TRIGGER_HURT"
+      "by some environment danger."
+    when "MOD_ROCKET_SPLASH"
+      "with a rocket explosion."
+    when "MOD_FALLING"
+      "by falling from a height enough to kill."
+    when "MOD_ROCKET"
+      "with a rocket."
+    when "MOD_RAILGUN"
+      "with a railgun."
+    when "MOD_MACHINEGUN"
+      "with a machinegun."
+    when "MOD_SHOTGUN"
+      "with a shotgun."
+    when "MOD_TELEFRAG"
+      "by telegraffing them."
+    when "MOD_BFG_SPLASH"
+      "with a BFG splash damage."
+    when "MOD_BFG"
+      "with a BFG."
+    when "MOD_CRUSH"
+      "by being crushed by a moving door or platform."
+    end
+  end
+
   def get_death_mean
     death_mean = DeathMean.find_by_name(@death_mean)
     if death_mean
       @death_mean = death_mean
     else
-      @death_mean = DeathMean.create(name: @death_mean)
+      death_description = get_death_mean_description
+      @death_mean = DeathMean.create(name: @death_mean, description: death_description)
     end
   end
 
