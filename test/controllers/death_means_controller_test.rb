033b51215c3e66077631701c1937866f898f80a6
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-27 08:53:07 -0300

    tests: implements tests for death_means controller

--- test/controllers/death_means_controller_test.rb
+++ test/controllers/death_means_controller_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class DeathMeansControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    get death_means_path
+  end
+
+  test "should get to the Means of Death page" do
+    assert_response :success
+    assert_select "h1", "Means of Death"
+  end
+
+  test "should show the Means of Death codes" do
+    assert_select "li", "Rocket"
+    assert_select "li", "Falling"
+  end
 end
